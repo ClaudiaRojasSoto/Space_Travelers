a8a2246bb523241f5bcb8f7300b4ce1e00f800d7
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-23 11:55:28 -0400

    Modify the App.js file importing the components, the BrowserRoute and React and creating new tag elements inside app function adding the nav with a list and inside it the Navlinks to rockest, missions and my-profile with their routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact activeClassName="active" to="/">
+                Rockets
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact activeClassName="active" to="/missions">
+                Missions
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact activeClassName="active" to="/my-profile">
+                My Profile
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Route exact path="/" component={Rockets} />
+        <Route exact path="/missions" component={Missions} />
+        <Route exact path="/my-profile" component={MyProfile} />
+      </div>
+    </Router>
   );
 }
 
