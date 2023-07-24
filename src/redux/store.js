ba3a530e2c099952a4b59c3d4caee28e886aa77e
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-24 17:14:50 -0400

    Create the file store.js adding inside ir the importations of configureStore and the store with the reducer for rockets

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+// redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './rockets/rocketsSlice';
+
+const store = configureStore({
+  reducer: {
+    rockets: rocketsReducer,
+  },
+});
+
+export default store;
