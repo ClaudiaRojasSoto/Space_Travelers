011774fcf1818162de02bfcf858dce8d249067f4
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-24 17:25:10 -0400

    Define the redux slice to handle the state of the rockets, set the initial state, the synchronous actions that will update that state, and the reducer that will update the state based on those actions

---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,52 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  rocketsData: [], // This will store the rockets data including id, name, type, flickr_images
+  loading: false,
+  error: null,
+};
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {
+    setRocketsData: (state, action) => {
+      state.rocketsData = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    setLoading: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    setError: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setRocketsData, setLoading, setError } = rocketsSlice.actions;
+
+export const fetchRocketsData = () => (dispatch) => {
+  dispatch(setLoading());
+  axios
+    .get('https://api.spacexdata.com/v3/rockets')
+    .then((response) => {
+      const rocketsData = response.data.map((rocket) => ({
+        id: rocket.id,
+        name: rocket.rocket_name,
+        type: rocket.rocket_type,
+        flickr_images: rocket.flickr_images,
+        // You can add more fields according to your needs
+      }));
+
+      dispatch(setRocketsData(rocketsData));
+    })
+    .catch((error) => {
+      dispatch(setError(error.message));
+    });
+};
+
+export default rocketsSlice.reducer;
