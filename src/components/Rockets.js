44f510db7a63be5e406738399280b09c5d0cb1e3
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-24 17:33:11 -0400

    Create the React component that display the rocket data in the user interface. Use the useSelector hook to get the data from the state stored in Redux and the useDispatch hook to dispatch the actions defined

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRocketsData } from '../redux/rockets/rocketsSlice';
 
-const Rockets = () => (
-  <div>
-    <h3>Rockets</h3>
-  </div>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.rockets.loading);
+  const error = useSelector((state) => state.rockets.error);
+  const rocketsData = useSelector((state) => state.rockets.rocketsData);
+
+  useEffect(() => {
+    dispatch(fetchRocketsData());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error:
+        {' '}
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>Rockets</h2>
+      <ul>
+        {rocketsData.map((rocket) => (
+          <li key={rocket.id}>
+            <h3>{rocket.rocket_name}</h3>
+            <p>
+              Type:
+              {rocket.type}
+            </p>
+            <p>
+              ID:
+              {rocket.id}
+            </p>
+            <p>
+              Description:
+              {rocket.description}
+            </p>
+            {rocket.flickr_images && (
+              <div>
+                <p>Images:</p>
+                <ul>
+                  {rocket.flickr_images.map((image) => (
+                    <li key={image}>
+                      <img src={image} alt="Rocket" />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
