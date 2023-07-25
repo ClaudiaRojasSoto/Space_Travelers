c80ac7823d460af7b94d84e49770d45654f3dc6f
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-25 18:34:30 -0400

    Use  useSelector to obtain rocketsData from redux state, use filter on rocketsData to obtain just the reserved rockets and use map on reservedRockets to generate the table rows that show the name of the  reserved rockets

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import '../styles/myProfile.css';
+import { fetchRocketsData } from '../redux/rockets/rocketsSlice';
 
-const MyProfile = () => (
-  <section className="myProfile">
-    <div>
-      <h3>My Missions</h3>
-    </div>
-    <div>
-      <h3>My Rockets</h3>
-    </div>
-  </section>
-);
+const MyProfile = () => {
+  const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
+  const rocketsData = useSelector((state) => state.rockets.rocketsData);
+  const reservedRockets = rocketsData.filter((rocket) => rocket.reserved);
+
+  useEffect(() => {
+    if (rocketsData.length === 0) {
+      dispatch(fetchRocketsData()).then(() => setLoaded(true));
+    } else {
+      setLoaded(true);
+    }
+  }, [dispatch, rocketsData]);
+
+  if (!loaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className="myProfile">
+      <div>
+        <h3>My Missions</h3>
+      </div>
+      <div style={{ textAlign: 'left' }}>
+        <h3 style={{ marginBottom: '10px' }}>My Rockets</h3>
+        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+          <tbody>
+            {reservedRockets.map((rocket) => (
+              <tr key={rocket.id}>
+                <td style={{ border: '1px solid #dedede', padding: '23px', backgroundColor: 'white' }}>{rocket.rocket_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
 
 export default MyProfile;
